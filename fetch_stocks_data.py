fa74d0b4712cdf3cc3a28242640e73d3002ed226
JP <jpsiyyadri@gmail.com>
2021-11-01 11:34:04 +0530

    added

---
+++ fetch_stocks_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import pandas_datareader.data as web
+import datetime as dt
+start = dt.datetime(1900, 5, 2)
+end = dt.datetime.today()
+df = web.DataReader(['TSLA','AAPL', 'RELIANCE.NS', 'TCS.NS', 'AMZN'], 'yahoo', start, end)
+df.to_csv("stocks_data.csv")
+print("data fetched succesfully")
