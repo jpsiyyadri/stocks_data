056c8dee441ce729b7adb689bbefe632b29c8189
JP <jpsiyyadri@gmail.com>
2021-11-01 11:35:15 +0530

    Update fetch_stocks_data.py

--- fetch_stocks_data.py
+++ fetch_stocks_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pandas_datareader.data as web
 import datetime as dt
-start = dt.datetime(1900, 5, 2)
+start = dt.datetime(1900, 1, 1)
 end = dt.datetime.today()
 df = web.DataReader(['TSLA','AAPL', 'RELIANCE.NS', 'TCS.NS', 'AMZN'], 'yahoo', start, end)
 df.to_csv("stocks_data.csv")
